ff5f485453b1288ca734c0a0353e27698261e193
Marlon Costa <marlonfcosta90@gmail.com>
2020-12-26 23:29:16 -0300

    Criacao dos componentes update e delete

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -33,6 +33,8 @@ import { MatSortModule } from '@angular/material/sort';
 
 import localePt from '@angular/common/locales/pt';
 import { registerLocaleData } from '@angular/common';
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
+import { ProductDeleteComponent } from './components/product/product-delete/product-delete.component';
 
 registerLocaleData(localePt);
 
@@ -48,7 +50,9 @@ registerLocaleData(localePt);
     ForDirective,
     ProductCreateComponent,
     ProductReadComponent,
-    ProductReadModelSchematicsComponent
+    ProductReadModelSchematicsComponent,
+    ProductUpdateComponent,
+    ProductDeleteComponent
   ],
   imports: [
     BrowserModule,
